068150f1685f41f115d1fa204851310f6f5d09bb
modestimpala <45338507+modestimpala@users.noreply.github.com>
2024-09-07 21:58:49 -0700

    fix fullTummy crash

--- VotV/Binaries/Win64/Mods/ChaosMod/Scripts/commands/fullTummy.lua
+++ VotV/Binaries/Win64/Mods/ChaosMod/Scripts/commands/fullTummy.lua
@@ -4,10 +4,8 @@ return {
     execute = function()
         local FirstPlayerController = UEHelpers.GetPlayerController()
         local Pawn = FirstPlayerController.Pawn
-        local cake = StaticFindObject("/Game/objects/prop_food_cake.prop_food_cake_C")
-        local out = {}
-        local out2 = nil
-        Pawn:addFood(100, 0, 0, cake, false, out)
+        local saveSlot = FindFirstOf("saveSlot_C")
+        saveSlot.food = 100
         return true
     end
 }
