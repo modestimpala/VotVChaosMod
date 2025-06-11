8e610e3448cdb15a369b875477ba34eaacb39d19
modestimpala <45338507+modestimpala@users.noreply.github.com>
2025-06-11 12:48:11 -0700

    feat: Introduce OBS Browser Overlay and enhance ChaosMod features - Added OBS Browser Overlay for real-time voting updates - Introduced "The Big Red Button" for triggering random events - Reworked Nextbot AI and added two new Nextbots - Updated version to 3.3.0 and modified several commands - Added new configuration options for overlay settings

--- VotV/Binaries/Win64/pyChaosMod/src/utils/config.py
+++ VotV/Binaries/Win64/pyChaosMod/src/utils/config.py
@@ -144,7 +144,7 @@ class AsyncConfigManager:
             config['files']['commands'] = os.path.join(self.base_path, 'twitchChannelPoints.cfg')
                        
         # VERSION
-        config['version'] = '3.2.0'
+        config['version'] = '3.3.0'
         # VERSION
         
         self.config = config
